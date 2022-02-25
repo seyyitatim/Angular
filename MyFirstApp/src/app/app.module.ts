1a69d31ed489d2cb807b0326d000b8693b2d9b95
seyyitatim <seyyitatim@gmail.com>
2022-02-25 18:07:34 +0300

    update project

--- MyFirstApp/src/app/app.module.ts
+++ MyFirstApp/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
+    CategoryComponent,
     ProductComponent
   ],
   imports: [
--- MyFirstApp/src/app/product/product.component.ts
+++ MyFirstApp/src/app/product/product.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html', 
-  // templateUrl'e bir html sayfası atadığımız gibi direk html koduda atayabiliriz.
-  // template:`<p>product name {{name}}</p>` şeklinde yaparsak da çalışır.
+  templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-
 export class ProductComponent implements OnInit {
 
-  name = "monster";
-
   constructor() { }
-
+  title = "Ürün Listesi";
+  products: any[] = [
+    { id: 1, name: "laptop", price: 2500, categoryId: 1, description: "Asus Zenbook" },
+    { id: 1, name: "mouse", price: 25, categoryId: 2, description: "lucitik" }
+  ];
+  
   ngOnInit(): void {
   }
 
