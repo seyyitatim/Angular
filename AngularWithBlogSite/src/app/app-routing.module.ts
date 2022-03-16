6df9daafff8c8a98d4c6b44932042f233c220c9d
seyyitatim <seyyitatim@gmail.com>
2022-03-16 10:41:59 +0300

    add routing

--- AngularWithBlogSite/src/app/app-routing.module.ts
+++ AngularWithBlogSite/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'hakkimizda',
+        component: AboutComponent,
+      },
+      {
+        path: 'iletisim',
+        component: ContactComponent,
+      },
+    ],
+  },
+  {
+    path: 'Admin',
+    component: AdminLayoutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
